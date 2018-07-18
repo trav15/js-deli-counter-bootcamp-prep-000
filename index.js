ab4655f390e8d968c7006c4761cb138764583a68
Travis Tokuyama <travistokuyama@gmail.com>
2018-07-18 03:00:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,9 @@ function nowServing(katzDeliLine) {
     return 'There is nobody waiting to be served!'
   }
 }
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length > 0){
+    for ()
+  }
+}
