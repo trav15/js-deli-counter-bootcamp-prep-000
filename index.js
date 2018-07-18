da1989d38473abd399d8e6e692abedf3269f29b9
Travis Tokuyama <travistokuyama@gmail.com>
2018-07-18 02:53:36 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+var katzDeli
+
+function takeANumber(katzDeliLine, newPerson) {
+  var placeInLine
+  katzDeliLine.push(newPerson)
+  placeInLine = katzDeliLine.length
+  return `Welcome, ${newPerson}. You are number ${placeInLine} in line.`
+}
